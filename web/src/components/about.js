0ffd858541cf09c50ad557cae5f5fb878cd3e076
Sepehr Hosseini <iamsepehrhosseini@gmail.com>
2020-05-06 11:40:55 +0400

    feat(about): improve about styles

--- web/src/components/about.js
+++ web/src/components/about.js
@@ -4,21 +4,23 @@ import styles from "./about.module.css";
 
 function About() {
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.content}>
-        <h2 className={styles.title}>About me</h2>
-        <p className={styles.para}>
-          I am a 21-year-old Full Stack Developer with 4+ years of experience based in Yerevan
-          working at LogicKit. I build{" "}
-          <strong>
-            <i>everything</i>
-          </strong>{" "}
-          from Backend to Frontend and{" "}
-          <strong>
-            <i>everything</i>
-          </strong>{" "}
-          in between.
-        </p>
+    <div className={styles.wrapper} id="about">
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <h2 className={styles.title}>About me</h2>
+          <p className={styles.para}>
+            I am a 21-year-old Full Stack Developer with 4+ years of experience based in Yerevan
+            working at LogicKit. I build{" "}
+            <strong>
+              <i>everything</i>
+            </strong>{" "}
+            from Backend to Frontend and{" "}
+            <strong>
+              <i>everything</i>
+            </strong>{" "}
+            in between.
+          </p>
+        </div>
       </div>
     </div>
   );
--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -9,7 +9,7 @@ import Container from "../components/container";
 import GraphQLErrorList from "../components/graphql-error-list";
 import ProjectPreviewGrid from "../components/project-preview-grid";
 import SEO from "../components/seo";
-import Layout from '../containers/layout'
+import Layout from "../containers/layout";
 import About from "../components/about";
 
 export const query = graphql`
@@ -90,14 +90,10 @@ const IndexPage = props => {
       <Container>
         <h1 hidden>Welcome to {site.title}</h1>
         {projectNodes && (
-          <ProjectPreviewGrid
-            title="Latest projects"
-            nodes={projectNodes}
-            browseMoreHref="/archive/"
-          />
+          <ProjectPreviewGrid title="Projects" nodes={projectNodes} browseMoreHref="/archive/" />
         )}
-        <About />
       </Container>
+      <About />
     </Layout>
   );
 };
